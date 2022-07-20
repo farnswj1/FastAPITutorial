8c7e7ad98f6b26fb75d50e7bab1c45b274e80334
Justin Farnsworth <farnswj1@tcnj.edu>
2022-07-20 17:15:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# FastAPI Tutorial
+This is a containerized FastAPI tutorial.
+
+## Setup
+The project uses the following:
+- Python 3.10
+- FastAPI
+- Nginx 1.21
+- Docker
+- Docker Compose
+
+For additional information on project specifications, see the ```Pipfile```.
+
+## Building
+The project uses Docker. Ensure Docker and Docker Compose are installed before continuing.
+
+To build, run ```docker-compose build```
+
+## Running
+To run the web app, run ```docker-compose up -d```, then 
+go to http://localhost using your web browser.
---
+++ api/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Person(BaseModel):
+    name: str
+    age: Union[int, None] = None
+
+
+@app.get('/')
+def root():
+    return {'Hello': 'World'}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {'item_id': item_id, 'q': q}
+
+
+@app.post('/greet/')
+def greet(person: Person):
+    greeting = f'Hello {person.name}.'
+    age = person.age
+
+    if age is not None and age >= 0:
+        greeting += f' You are {age} year old!' if age == 1 else f' You are {age} years old!'
+    else:
+        greeting += ' How old are you?'
+
+    return {'greeting': greeting}
