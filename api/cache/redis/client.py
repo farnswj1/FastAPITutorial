c6250f9dfbc8bc6b6e233897bd1ba344b09eb88d
Justin Farnsworth <farnswj1@tcnj.edu>
2022-09-16 15:56:06 -0400

    allow default value for get method

--- api/cache/redis/client.py
+++ api/cache/redis/client.py
@@ -1,8 +1,11 @@
 from redis.asyncio import Redis
 from redis.commands.core import ResponseT
 from config import settings
-from typing import Any
+from typing import Any, TypeAlias
 import pickle
+
+
+_Key: TypeAlias = str | bytes
 
 
 class RedisCache(Redis):
@@ -11,22 +14,19 @@ class RedisCache(Redis):
         return value if type(value) is int else pickle.dumps(value, pickle.HIGHEST_PROTOCOL)
 
     @staticmethod
-    def deserialize(value: str | bytes | None = None) -> Any:
-        if value is None:
-            return value
-
+    def deserialize(value: _Key | None = None) -> Any:
         try:
             return int(value)
         except ValueError:
             return pickle.loads(value)
 
-    async def set(self, name: str | bytes, value: Any, *args, **kwargs) -> ResponseT:
+    async def set(self, name: _Key, value: Any, *args, **kwargs) -> ResponseT:
         _value = self.serialize(value)
         return await super().set(name, _value, *args, **kwargs)
 
-    async def get(self, name: str | bytes) -> Any:
+    async def get(self, name: _Key, default: Any = None) -> Any:
         value = await super().get(name)
-        return self.deserialize(value)
+        return default if value is None else self.deserialize(value)
 
 
 redis = RedisCache.from_url(settings.REDIS_URL)
